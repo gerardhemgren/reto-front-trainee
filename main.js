5271e4c79a5bae960abe18f08e16e582db87da07
gerard hemgren <gerardhemgren@gmail.com>
2022-04-22 14:36:23 -0300

    model with pokemon object & array of pokemons

--- main.js
+++ main.js
@@ -2,13 +2,26 @@ const model = {
     params: {
         baseUrl: 'https://pokeapi.co/api/v2/pokemon',
         limit: 9,
-        offsetApiUrl: null
+        offsetApiUrl: null,
+        offset: null
+    },
+
+    listOfPokemons: [],
+
+    pokemon: {
+        id: null,
+        name: null,
+        weight: null,
+        base_experience: null,
+        img: null,
+        types: [],
+        stats: []
     }
 }
 
 const view = {
-    showPokemon: function (idOrName) {
-        controller.getPokemonByIdOrName(idOrName);
+    showPokemon: function (idOrName, source) {
+        controller.getPokemonByIdOrName(idOrName, source);
     },
 
     showListOfPokemons: function () {
@@ -17,10 +30,42 @@ const view = {
 }
 
 const controller = {
-    getPokemonByIdOrName: async function (idOrName) {
+    getPokemonByIdOrName: async function (idOrName, source) {
         let response = await fetch(model.params.baseUrl + '/' + idOrName);
         let data = await response.json();
-        console.log(data.name);
+
+        let pokemon = {
+            id: null,
+            name: null,
+            weight: null,
+            base_experience: null,
+            img: null,
+            types: [],
+            stats: []
+        }
+
+        pokemon.id = data.id;
+        pokemon.name = data.name;
+        pokemon.weight = data.weight;
+        pokemon.base_experience = data.base_experience;
+        pokemon.img = data.sprites.other["official-artwork"].front_default;
+        pokemon.types = data.types;
+        pokemon.stats = [
+            { name: data.stats[0].stat.name, stat: data.stats[0].base_stat },
+            { name: data.stats[1].stat.name, stat: data.stats[1].base_stat },
+            { name: data.stats[2].stat.name, stat: data.stats[2].base_stat },
+            { name: data.stats[5].stat.name, stat: data.stats[5].base_stat },
+        ]
+
+        model.pokemon = pokemon;
+        model.listOfPokemons = [...model.listOfPokemons, pokemon];
+
+        if (model.listOfPokemons.length === model.params.offset) {
+            console.log(model.listOfPokemons);
+        }
+        if (source === 'show_pokemon') {
+            console.log(pokemon);
+        }
     },
 
     getListOfPokemons: async function () {
@@ -33,8 +78,13 @@ const controller = {
             response = await fetch(firstCallUrl)
             ;
         let data = await response.json();
+
         model.params.offsetApiUrl = data.next;
-        console.log(data.results)
+
+        for (let pokemon in data.results) {
+            model.params.offset += 1;
+            this.getPokemonByIdOrName(data.results[pokemon].name);
+        }
     }
 }
 
